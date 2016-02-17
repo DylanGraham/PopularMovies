9899b745bd655dd79f7dcd2f9227063ffae054d6
Dylan Graham <dylan.graham@gmail.com>
2016-02-17 18:26:26 +1100

    No TextView on movieitem

--- app/src/main/java/org/dylangraham/popularmovies/MovieAdapter.java
+++ app/src/main/java/org/dylangraham/popularmovies/MovieAdapter.java
@@ -6,7 +6,6 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.ImageView;
-import android.widget.TextView;
 
 import java.util.List;
 
@@ -37,9 +36,6 @@ public class MovieAdapter extends ArrayAdapter<MovieItem> {
 
         ImageView iconView = (ImageView) convertView.findViewById(R.id.movie_image);
         iconView.setImageResource(mi.image);
-
-        TextView ratingView = (TextView) convertView.findViewById(R.id.movie_text);
-        ratingView.setText(mi.rating);
 
         return convertView;
     }
