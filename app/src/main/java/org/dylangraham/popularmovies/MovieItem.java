d9e968615056cfab3bc1ab18ba6380fa8358464e
Dylan Graham <dylan.graham@gmail.com>
2016-03-01 21:31:54 +1100

    Implement Parcelable

--- app/src/main/java/org/dylangraham/popularmovies/MovieItem.java
+++ app/src/main/java/org/dylangraham/popularmovies/MovieItem.java
@@ -1,6 +1,9 @@
 package org.dylangraham.popularmovies;
 
-public class MovieItem {
+import android.os.Parcel;
+import android.os.Parcelable;
+
+public class MovieItem implements Parcelable {
     String movieName;
     String rating;
     String imageURL;
@@ -11,4 +14,33 @@ public class MovieItem {
         this.imageURL = imageURL;
     }
 
-}
+    private MovieItem(Parcel in) {
+        movieName = in.readString();
+        rating = in.readString();
+        imageURL = in.readString();
+    }
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    public static final Parcelable.Creator<MovieItem> CREATOR = new Parcelable.Creator<MovieItem>() {
+        @Override
+        public MovieItem[] newArray(int size) {
+            return new MovieItem[size];
+        }
+
+        public MovieItem createFromParcel(Parcel in) {
+            return new MovieItem(in);
+        }
+    };
+
+        public void writeToParcel(Parcel dest, int flags) {
+            dest.writeString(movieName);
+            dest.writeString(rating);
+            dest.writeString(imageURL);
+        }
+
+
+    }
