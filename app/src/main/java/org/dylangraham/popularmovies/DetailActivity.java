0c6d22b2a25f51e2481931b07dac69cdc423d4f9
Dylan Graham <dylan.graham@gmail.com>
2016-03-02 21:32:53 +1100

    Grab Parcel and display Toast

--- app/src/main/java/org/dylangraham/popularmovies/DetailActivity.java
+++ app/src/main/java/org/dylangraham/popularmovies/DetailActivity.java
@@ -1,7 +1,8 @@
 package org.dylangraham.popularmovies;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.Toast;
 
 public class DetailActivity extends AppCompatActivity {
 
@@ -9,5 +10,13 @@ public class DetailActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detail);
+
+        Bundle b = getIntent().getExtras();
+        MovieItem movieItem = b.getParcelable("movieItemsParcel");
+        if (movieItem != null) {
+            Toast toast = Toast.makeText(this, movieItem.movieName, Toast.LENGTH_SHORT);
+        toast.show();
+    }
+
     }
 }
