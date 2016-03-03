51a887d101870c733d4086ba4b3cf5a988c46d19
Dylan Graham <dylan.graham@gmail.com>
2016-03-03 22:01:21 +1100

    Remove movieItem null check

--- app/src/main/java/org/dylangraham/popularmovies/DetailActivity.java
+++ app/src/main/java/org/dylangraham/popularmovies/DetailActivity.java
@@ -13,10 +13,7 @@ public class DetailActivity extends AppCompatActivity {
 
         Bundle b = getIntent().getExtras();
         MovieItem movieItem = b.getParcelable("movieItemsParcel");
-        if (movieItem != null) {
-            Toast toast = Toast.makeText(this, movieItem.movieName, Toast.LENGTH_SHORT);
+        Toast toast = Toast.makeText(this, movieItem.movieName, Toast.LENGTH_SHORT);
         toast.show();
-    }
-
     }
 }
