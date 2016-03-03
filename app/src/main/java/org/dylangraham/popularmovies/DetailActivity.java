6caa0b5084e1566f84d2495d347042db0b8019e4
Dylan Graham <dylan.graham@gmail.com>
2016-03-03 22:18:18 +1100

    Set ActionBar title

--- app/src/main/java/org/dylangraham/popularmovies/DetailActivity.java
+++ app/src/main/java/org/dylangraham/popularmovies/DetailActivity.java
@@ -2,18 +2,18 @@ package org.dylangraham.popularmovies;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.widget.Toast;
 
 public class DetailActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_detail);
 
         Bundle b = getIntent().getExtras();
         MovieItem movieItem = b.getParcelable("movieItemsParcel");
-        Toast toast = Toast.makeText(this, movieItem.movieName, Toast.LENGTH_SHORT);
-        toast.show();
+        if (getSupportActionBar() != null && movieItem != null) {
+            getSupportActionBar().setTitle(movieItem.movieName);
+        }
+        setContentView(R.layout.activity_detail);
     }
 }
