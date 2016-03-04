63a8721bc6f09b231e12741658a5c004efbdcb0b
Dylan Graham <dylan.graham@gmail.com>
2016-03-04 13:17:08 +1100

    First 2 views

--- app/src/main/java/org/dylangraham/popularmovies/DetailActivity.java
+++ app/src/main/java/org/dylangraham/popularmovies/DetailActivity.java
@@ -2,18 +2,34 @@ package org.dylangraham.popularmovies;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.squareup.picasso.Picasso;
+
+import org.w3c.dom.Text;
 
 public class DetailActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detail);
 
         Bundle b = getIntent().getExtras();
         MovieItem movieItem = b.getParcelable("movieItemsParcel");
         if (getSupportActionBar() != null && movieItem != null) {
             getSupportActionBar().setTitle(movieItem.movieName);
+
+            TextView title = (TextView) findViewById(R.id.detail_title);
+            title.setText(movieItem.movieName);
+
+            ImageView detailImage = (ImageView) findViewById(R.id.detail_image);
+            Picasso.with(this)
+                    .load(movieItem.backdropURL)
+                    .error(R.mipmap.ic_launcher)
+                    .into(detailImage);
         }
-        setContentView(R.layout.activity_detail);
+
     }
 }
