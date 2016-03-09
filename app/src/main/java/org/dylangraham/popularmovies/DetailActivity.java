fed963e6a1e11c7ff9db16a3a381f1ea2a4fe43f
Dylan Graham <dylan.graham@gmail.com>
2016-03-09 15:27:20 +1100

    Add picassopalette

--- app/src/main/java/org/dylangraham/popularmovies/DetailActivity.java
+++ app/src/main/java/org/dylangraham/popularmovies/DetailActivity.java
@@ -1,10 +1,18 @@
 package org.dylangraham.popularmovies;
 
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.graphics.Palette;
+import android.text.Layout;
 import android.widget.ImageView;
+import android.widget.RelativeLayout;
+import android.widget.ScrollView;
 import android.widget.TextView;
 
+import com.github.florent37.picassopalette.BitmapPalette;
+import com.github.florent37.picassopalette.PicassoPalette;
 import com.squareup.picasso.Picasso;
 
 public class DetailActivity extends AppCompatActivity {
@@ -19,6 +27,9 @@ public class DetailActivity extends AppCompatActivity {
         if (getSupportActionBar() != null && movieItem != null) {
             getSupportActionBar().setTitle(movieItem.movieName);
 
+            RelativeLayout detailLayout = (RelativeLayout) findViewById(R.id.relative_layout_detail);
+            ScrollView detailScroll = (ScrollView) findViewById(R.id.scrollview_detail);
+
             TextView title = (TextView) findViewById(R.id.detail_title);
             title.setText(movieItem.movieName);
 
@@ -29,10 +40,13 @@ public class DetailActivity extends AppCompatActivity {
             overview.setText(movieItem.overview);
 
             ImageView poster = (ImageView) findViewById(R.id.detail_poster);
-            Picasso.with(this)
-                    .load(movieItem.imageURL)
-                    .error(R.mipmap.ic_launcher)
-                    .into(poster);
+            Picasso.with(this).load(movieItem.imageURL).error(R.mipmap.ic_launcher).into(poster,
+                    PicassoPalette.with(movieItem.imageURL, poster)
+                            .use(PicassoPalette.Profile.MUTED_DARK)
+                            .intoBackground(detailScroll)
+                            .intoTextColor(title, BitmapPalette.Swatch.TITLE_TEXT_COLOR)
+                            .intoTextColor(average, BitmapPalette.Swatch.TITLE_TEXT_COLOR)
+                            .intoTextColor(overview, BitmapPalette.Swatch.BODY_TEXT_COLOR));
 
             ImageView detailImage = (ImageView) findViewById(R.id.detail_image);
             Picasso.with(this)
