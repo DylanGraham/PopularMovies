b435181fcd93a3a3285a8f05677ec19644d0fb2c
Dylan Graham <dylan.graham@gmail.com>
2016-03-18 13:14:35 +1100

    Change app name, remove action bar lookup

--- app/src/main/java/org/dylangraham/popularmovies/MainActivity.java
+++ app/src/main/java/org/dylangraham/popularmovies/MainActivity.java
@@ -8,9 +8,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getSupportActionBar() != null) {
-            getSupportActionBar().setTitle("Popular Movies");
-        }
+
         setContentView(R.layout.activity_main);
     }
 }
