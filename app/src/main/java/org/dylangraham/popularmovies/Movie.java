e966807b3ef67995b995408c9a974cec1278a368
Dylan Graham <dylan.graham@gmail.com>
2016-03-21 09:23:29 +1100

    setters

--- app/src/main/java/org/dylangraham/popularmovies/Movie.java
+++ app/src/main/java/org/dylangraham/popularmovies/Movie.java
@@ -1,5 +1,6 @@
 package org.dylangraham.popularmovies;
 
+
 public class Movie {
     String title;
     String vote_average;
@@ -13,27 +14,55 @@ public class Movie {
         return title;
     }
 
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
     public String getVote_average() {
         return vote_average;
+    }
+
+    public void setVote_average(String vote_average) {
+        this.vote_average = vote_average;
     }
 
     public String getPoster_path() {
         return poster_path;
     }
 
+    public void setPoster_path(String poster_path) {
+        this.poster_path = poster_path;
+    }
+
     public String getBackdrop_path() {
         return backdrop_path;
+    }
+
+    public void setBackdrop_path(String backdrop_path) {
+        this.backdrop_path = backdrop_path;
     }
 
     public String getOverview() {
         return overview;
     }
 
+    public void setOverview(String overview) {
+        this.overview = overview;
+    }
+
     public String getPopularity() {
         return popularity;
     }
 
+    public void setPopularity(String popularity) {
+        this.popularity = popularity;
+    }
+
     public String getRelease_date() {
         return release_date;
+    }
+
+    public void setRelease_date(String release_date) {
+        this.release_date = release_date;
     }
 }
