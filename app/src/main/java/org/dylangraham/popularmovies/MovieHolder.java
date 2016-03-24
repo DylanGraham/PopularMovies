e78aa3924ce627b5ec3da0973ca0dff3c2642e1e
Dylan Graham <dylan.graham@gmail.com>
2016-03-24 14:55:49 +1100

    onClickListener for itemView

--- app/src/main/java/org/dylangraham/popularmovies/MovieHolder.java
+++ app/src/main/java/org/dylangraham/popularmovies/MovieHolder.java
@@ -1,5 +1,7 @@
 package org.dylangraham.popularmovies;
 
+import android.content.Context;
+import android.content.Intent;
 import android.support.v7.widget.RecyclerView;
 import android.view.View;
 import android.widget.ImageView;
@@ -9,18 +11,21 @@ public class MovieHolder extends RecyclerView.ViewHolder {
     private MovieItem movieItem;
     private ImageView movieImage;
 
-    public MovieHolder(View itemView) {
+    public MovieHolder(final View itemView) {
         super(itemView);
 
         movieImage = (ImageView) itemView.findViewById(R.id.movie_image);
 
-/*        movieItem.setOnClickListener(new View.OnClickListener() {
+        itemView.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                Context context = itemView.getContext();
+
                 Intent detailIntent = new Intent(context, DetailActivity.class);
                 detailIntent.putExtra("movieItemsParcel", movieItem);
                 context.startActivity(detailIntent);
-            }*/
+            }
+        });
     }
 
     public void bindMovie(MovieItem movieItem) {
