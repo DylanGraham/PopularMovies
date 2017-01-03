9c61cde420cb8a935cf90e3a028b78ab51124a49
Dylan Graham <dylan.graham@gmail.com>
2017-01-03 15:15:45 +1100

    messing with the palette

--- app/src/main/java/org/dylangraham/popularmovies/DetailActivity.java
+++ app/src/main/java/org/dylangraham/popularmovies/DetailActivity.java
@@ -54,9 +54,9 @@ public class DetailActivity extends AppCompatActivity {
             ImageView poster = (ImageView) findViewById(R.id.detail_poster);
             Picasso.with(this).load(movieItem.imageURL).error(R.mipmap.ic_launcher).into(poster,
                     PicassoPalette.with(movieItem.imageURL, poster)
-                            .use(PicassoPalette.Profile.MUTED)
+                            .use(PicassoPalette.Profile.MUTED_LIGHT)
                             .intoBackground(detailScroll)
-                            .intoTextColor(title, PicassoPalette.Swatch.TITLE_TEXT_COLOR)
+                            .intoTextColor(title, PicassoPalette.Swatch.BODY_TEXT_COLOR)
                             .intoTextColor(year, PicassoPalette.Swatch.BODY_TEXT_COLOR)
                             .intoTextColor(average, PicassoPalette.Swatch.BODY_TEXT_COLOR)
                             .intoTextColor(overview, PicassoPalette.Swatch.BODY_TEXT_COLOR));
